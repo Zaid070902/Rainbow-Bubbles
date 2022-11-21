9f4b72e5858d9c8a454f5372643489580049f267
zaid070902 <zaidflandorp4@gmail.com>
2022-11-21 11:37:16 +0200

    done but with errors

---
+++ main.js
@@ -0,0 +1,25 @@
+let body = document.querySelector("body");
+
+function spawnPoint() {
+  let randomPosition = Math.random() * 90;
+  return Math.floor(randomPosition);
+}
+
+function movePoint() {
+  let randomPoint = Math.random() * 100;
+  return Math.floor(randomPoint);
+}
+
+spawnPoint();
+
+setInterval(function call(event) {
+  window.addEventListener("mousemove", call);
+
+  let circle = document.createElement("div");
+  circle.classList.add("circle");
+  circle.style.borderRadius = "50%";
+  circle.style.top = event.pageY + "px";
+  circle.style.left = event.pageX + "px";
+
+  body.appendChild(circle);
+}, 0);
